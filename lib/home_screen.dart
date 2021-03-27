9a5b70544a3bd4f6835317eb0485c6517533958a
mahesh jamdade <maheshmn121@gmail.com>
2021-03-27 09:07:17 +0530

    add callback for video progress

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:video_player/video_player.dart';
 import 'package:video_player_demo/video_items.dart';
 
 class HomeScreen extends StatelessWidget {
+  final percentNotifier = ValueNotifier<int>(0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,20 +20,30 @@ class HomeScreen extends StatelessWidget {
                 'https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'),
             looping: false,
             autoplay: false,
+            progressListener: (currentPostion, percent) {
+              // print('Video at $currentPostion percent=$percent%');
+              percentNotifier.value = percent;
+            },
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
                 height: MediaQuery.of(context).size.height * 0.2,
                 width: MediaQuery.of(context).size.width * 0.5,
-                child: Card(child: Padding(
+                child: Card(
+                    child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Text("percentage watched: "+ "%0"),
-                      Text("positions watched: " + "[[0,0]]"),
-                    ],
-                  ),
+                  child: ValueListenableBuilder<int>(
+                      valueListenable: percentNotifier,
+                      builder:
+                          (BuildContext context, int percent, Widget child) {
+                        return Column(
+                          children: [
+                            Text("percentage watched: " + "%$percent"),
+                            Text("positions watched: " + "[[0,0]]"),
+                          ],
+                        );
+                      }),
                 ))),
           )
         ],
--- lib/video_items.dart
+++ lib/video_items.dart
@@ -6,11 +6,13 @@ class VideoItems extends StatefulWidget {
   final VideoPlayerController videoPlayerController;
   final bool looping;
   final bool autoplay;
+  final Function(Duration, int) progressListener;
 
   VideoItems(
       {@required this.videoPlayerController,
       this.looping,
       this.autoplay,
+      this.progressListener,
       Key key})
       : super(key: key);
 
@@ -20,17 +22,32 @@ class VideoItems extends StatefulWidget {
 
 class _VideoItemsState extends State<VideoItems> {
   ChewieController _chewieController;
-
+  int durationInSeconds;
+  int currentPositionInSeconds;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    widget.videoPlayerController.initialize().then((x) {
+      durationInSeconds = widget.videoPlayerController.value.duration.inSeconds;
+      widget.videoPlayerController.addListener(() {
+        final currentPosition = widget.videoPlayerController.value.position;
+        currentPositionInSeconds = currentPosition.inSeconds;
+        double progressPercent =
+            (currentPositionInSeconds / durationInSeconds) * 100;
+        widget.progressListener(currentPosition, progressPercent.toInt());
+      });
+    });
+
     _chewieController = ChewieController(
         videoPlayerController: widget.videoPlayerController,
         aspectRatio: 3 / 5,
         autoInitialize: true,
         autoPlay: widget.autoplay,
         looping: widget.looping,
+        showControls: true,
+        allowPlaybackSpeedChanging: true,
         errorBuilder: (context, errorMesaage) {
           return Center(
             child: Text(
